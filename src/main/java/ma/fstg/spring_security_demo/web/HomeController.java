ad89ddfbfd94edcb86847e672c6688192e8e1a82
Aouaq <128398427+Aouaq@users.noreply.github.com>
2026-06-13 10:12:24 +0100

    Initial commit

---
+++ src/main/java/ma/fstg/spring_security_demo/web/HomeController.java
@@ -0,0 +1,30 @@
+package ma.fstg.spring_security_demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Page d'accueil accessible après connexion.";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/dashboard")
+    public String userDashboard() {
+        return "Espace utilisateur - accessible aux rôles USER et ADMIN.";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard() {
+        return "Espace administrateur - réservé au rôle ADMIN.";
+    }
+
+
+}
