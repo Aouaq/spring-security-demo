75122e7d7214004f98958b6c71510400b14b11c0
Aouaq <128398427+Aouaq@users.noreply.github.com>
2026-06-13 10:42:24 +0100

    push

--- src/main/java/ma/fstg/spring_security_demo/web/HomeController.java
+++ src/main/java/ma/fstg/spring_security_demo/web/HomeController.java
@@ -1,9 +1,9 @@
 package ma.fstg.spring_security_demo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
